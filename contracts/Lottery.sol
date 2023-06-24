42f64e6f2522f0dc6dadb087df0db810a3c8e6e5
ElizabethOgbee <ogbeititi345@gmail.com>
2023-06-24 17:36:27 +0100

    added entranceFee function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,4 +2,25 @@
 
 pragma solidity ^0.8.0;
 
-contract lOTTERY{}
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+contract Lottery{
+    address payable[] public player;
+    //MAIN FUNCTIONS FOR OUR LOTTERY GAME: ENTER(), STARTGAME(), ENDGAME(), WITHDRAWAMOUNT()
+    constructor() public{
+        usdEntryFee = 50 * (10 ** 18);
+    }
+    function get entranceFee() public view returns(uint256){}
+    function enterGame() public payable {
+        players.push(msg.sender);
+    }
+
+    function startGame()public{}
+
+    function endGame()public{}
+
+    function withdrawGame()public{}
+
+
+}
+
+
